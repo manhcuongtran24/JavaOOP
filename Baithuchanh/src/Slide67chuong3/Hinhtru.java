ca40d4885a8b1ffeb0b6d66a5241646809f7740f
manhcuongtran24 <manhcuongtran06022004@gmail.com>
2024-04-20 07:16:00 +0700

    slide 67 chuong3

---
+++ Baithuchanh/src/Slide67chuong3/Hinhtru.java
@@ -0,0 +1,8 @@
+package Slide67chuong3;
+
+public class Hinhtru extends Hinhtron {
+    @Override
+    public void xuatThongTin() {
+        System.out.println("Day la Hinh Tru");
+    }
+}
---
+++ Baithuchanh/src/Slide67chuong3/MainClass.java
@@ -0,0 +1,10 @@
+package Slide67chuong3;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Hinhtron htron = new Hinhtron();
+        Hinhtru htru = new Hinhtru();
+        htron.xuatThongTin();
+        htru.xuatThongTin();
+    }
+}
