ee12230280dadf3a66550adb93836a76ecd2e5f9
manhcuongtran24 <manhcuongtran06022004@gmail.com>
2024-05-16 21:55:51 +0700

    slide 54 chuong 4

--- Baithuchanh/src/Slide54chuong4.java
+++ Baithuchanh/src/Slide54chuong4.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Slide55chuong4 {
+public class Slide54chuong4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HashSet<Integer> hashSetInteger = new HashSet<>();
